77b66b84f5c439137c26432d5687a55d2a39f820
kai1812 <kai1812@gmail.com>
2017-03-10 20:53:00 +0800

    點餐系統

---
+++ CS280-Lecture3-Examples/Example02/Program.cs
@@ -0,0 +1,23 @@
+﻿using StoreLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Example02
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Products 豆漿 = new Products();
+            豆漿.Name = "永和豆漿";
+            豆漿.Price = 15;
+
+            Products 油條 = new Products();
+            油條.Name = "永和油條";
+            油條.Price = 10;
+        }
+    }
+}
---
+++ CS280-Lecture3-Examples/Example03/Form1.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace Example03
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器
+        /// 修改這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.product1Button = new System.Windows.Forms.Button();
+            this.product2Button = new System.Windows.Forms.Button();
+            this.customerLabel = new System.Windows.Forms.Label();
+            this.orderRichtextbox = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // product1Button
+            // 
+            this.product1Button.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.product1Button.Location = new System.Drawing.Point(44, 41);
+            this.product1Button.Name = "product1Button";
+            this.product1Button.Size = new System.Drawing.Size(121, 74);
+            this.product1Button.TabIndex = 0;
+            this.product1Button.Text = "button1";
+            this.product1Button.UseVisualStyleBackColor = true;
+            // 
+            // product2Button
+            // 
+            this.product2Button.Location = new System.Drawing.Point(171, 41);
+            this.product2Button.Name = "product2Button";
+            this.product2Button.Size = new System.Drawing.Size(124, 74);
+            this.product2Button.TabIndex = 1;
+            this.product2Button.Text = "button2";
+            this.product2Button.UseVisualStyleBackColor = true;
+            // 
+            // customerLabel
+            // 
+            this.customerLabel.AutoSize = true;
+            this.customerLabel.Location = new System.Drawing.Point(44, 23);
+            this.customerLabel.Name = "customerLabel";
+            this.customerLabel.Size = new System.Drawing.Size(33, 12);
+            this.customerLabel.TabIndex = 2;
+            this.customerLabel.Text = "label1";
+            // 
+            // orderRichtextbox
+            // 
+            this.orderRichtextbox.Location = new System.Drawing.Point(46, 122);
+            this.orderRichtextbox.Name = "orderRichtextbox";
+            this.orderRichtextbox.Size = new System.Drawing.Size(463, 205);
+            this.orderRichtextbox.TabIndex = 3;
+            this.orderRichtextbox.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(546, 433);
+            this.Controls.Add(this.orderRichtextbox);
+            this.Controls.Add(this.customerLabel);
+            this.Controls.Add(this.product2Button);
+            this.Controls.Add(this.product1Button);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button product1Button;
+        private System.Windows.Forms.Button product2Button;
+        private System.Windows.Forms.Label customerLabel;
+        private System.Windows.Forms.RichTextBox orderRichtextbox;
+    }
+}
+
